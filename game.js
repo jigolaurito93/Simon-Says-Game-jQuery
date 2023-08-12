c308e31843da95eb55ecafeb313c85c3ae6c3b35
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-12 03:00:34 -0500

    created array from color list, empty array to push chose random color, function to generate random number

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-alert('hello')
+// Button Color for sequence
+var buttonColours = ['red', 'blue', 'green', 'yellow'];
+
+var gamePattern = []
+
+// Function that generates number from 0 to 3
+function nextSequence(){
+    var randomNumber = Math.round(Math.random()*3);
+    return randomNumber
+}
+
+// Randomly chooses a color from the buttonCoulours array
+var randomChosenColour = buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour)
+console.log(gamePattern); 
