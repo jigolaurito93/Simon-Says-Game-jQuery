40b0311106b6645d689620ce532728b1364c1faf
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-13 02:33:52 -0500

    created empty array to store colors that has been clicked to make a pattern

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+// Patern of colors that the user has clicked
+var userClickedPattern = []
+
 // Button Color for sequence
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
 
@@ -12,5 +15,18 @@ function nextSequence(){
 // Randomly chooses a color from the buttonCoulours array
 var randomChosenColour = buttonColours[nextSequence()];
 
-gamePattern.push(randomChosenColour)
-console.log(gamePattern); 
+gamePattern.push(randomChosenColour);
+
+console.log(gamePattern)
+
+$('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+
+var audio = new Audio('/sounds/' + randomChosenColour + '.mp3');
+// audio.play();
+
+$(document).on('click', function(e){
+    var userChosenCoulour = e.target.id // Grab the id of the chosen color
+    userClickedPattern.push(userChosenCoulour); // Add the id of the chosen color to the userClickedPattern array
+    console.log(userClickedPattern)
+})
+
