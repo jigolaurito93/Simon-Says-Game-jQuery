d0dc32eef1bbe1f4da2426bba1b40fc5d5c581da
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-13 03:11:29 -0500

    created animatePress func to add animation to button when clicked, used setTimeout function to delay an animation

--- game.js
+++ game.js
@@ -12,7 +12,8 @@ $(".btn").click(function(e){
     userClickedPattern.push(userChosenCoulour); // Add the id of the chosen color to the userClickedPattern array
 
     playSound(userChosenCoulour)
-    console.log(userClickedPattern)
+    animatePress(userChosenCoulour)
+    
 })
 
 // Function that generates number from 0 to 3
@@ -24,10 +25,22 @@ function nextSequence(){
     $('#'+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playSound(randomChosenCoulour);
+    animatePress(randomChosenCoulour)
 }
 
 // Function that plays the sound of the button when clicked
 function playSound(name){
     var audio = new Audio('/sounds/' + name + '.mp3');
     audio.play() //Play the appropriate audio of the button that was clicked
+}
+
+// Function to animate button when pressed
+// Used setTimeout to delay the animation
+function animatePress(currentColour){
+    $('#' + currentColour).addClass('pressed');
+    setTimeout(function(){
+        $('#' + currentColour).removeClass('pressed');
+    }, 100)
+    
+
 }
